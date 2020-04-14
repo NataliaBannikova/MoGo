87bc1351c6e731205e1af594d7c2ffb30c0c4eec
Natalia Bannikova <natabannikova10@gmail.com>
2020-04-14 11:20:40 +0300

    smooth scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@ $(function() {
 		introH = $("#intro").innerHeight(),
 		scrollOffset = $(window).scrollTop();
 
+	/* Fixed Header */
+
 	checkScroll(scrollOffset);
 
 	$(window).on("scroll", function() {
@@ -22,4 +24,28 @@ $(function() {
 			header.removeClass("fixed");
 		}
 	}
+
+
+	/* Smooth scroll */
+
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
+
+			$("html, body").animate({
+				scrollTop: blockOffset
+			}, 500);
+
+	});
+
+
+
 });
+
