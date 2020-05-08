e31acfd27e32fa7058c95e884b987a773bcce0e1
Natalia Bannikova <natabannikova10@gmail.com>
2020-05-08 17:06:41 +0300

    Accordion collapse

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,5 +55,16 @@ $(function() {
 
 	});
 
+	/* Accordion collapse */
+	$("[data-collapse]").on("click", function(event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('collapse');
+
+
+		$(this).toggleClass("active");
+	});
+
 });
 
